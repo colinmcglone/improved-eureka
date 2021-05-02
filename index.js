9e0ba33373cc067c55cd7723acd086ae176842c0
Colin McGlone <me@colinmcglone.ca>
2021-05-02 13:24:10 -0400

    t

--- index.js
+++ index.js
@@ -2,29 +2,29 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const PORT = 3000;
-const {} = require('./games')
-const {addUser, getUser, deleteUser, getUsers} = require('./users')
+const {} = require('./games');
+const {addUser, getUser, deleteUser, getUsers} = require('./users');
 
 
 io.on('connection', (socket) => {
   socket.on("login", ({name, room}, callback) => {
   
-  })
+  });
   socket.on("sendMessage", message => {
   
-  })
+  });
   socket.on("disconnect", () => {
   
-  })
-})
+  });
+});
 
 app.get('/', (req, res) => {
   req.send('Server is up and running')
-})
+});
 
 http.listen(PORT, () => {
   console.log(`Listening to ${PORT}`);
-})
+});
 
 http.listen(3000, () => {
   console.log('listening on *:3000');
