37fabc299942cb8718f7e4d4e6f9a482f54d62d0
Colin McGlone <me@colinmcglone.ca>
2021-05-02 13:17:23 -0400

    t

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('express')();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(3000);
 const {} = require('./games')
 const {addUser, getUser, deleteUser, getUsers} = require('./users')
 
